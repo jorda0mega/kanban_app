1ec3bea34e444a5b2eddaa5a2d33304348d231bc
jorda0mega <jorda0mega@gmail.com>
2015-08-22 17:55:11 -0400

    introducing react and babel to the kanban app

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
+var merge = require("webpack-merge");
+
+// gets the path to this app's directory
+var ROOT_PATH = path.resolve(__dirname);
+
+// gets the target on the npm lifecycle
+var TARGET = process.npm_lifecycle_event;
+
+var common = {
+	entry: path.resolve(ROOT_PATH, "app/main.jsx"),
+	devServer: {
+		colors: true,
+		historyApiFallback: true,
+		hot: true,
+		inline: true,
+		progress: true
+	},
+	output: {
+		path: path.resolve(ROOT_PATH, "build"),
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loaders: ["style","css"],
+				include: path.resolve(ROOT_PATH, "app")
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Kanban App"
+		})
+	]
+};
+
+if(TARGET == "start" || !TARGET){
+	module.exports = merge(common, {
+		devtool: "eval",
+		module: {
+			loaders: {
+				test: /\.jsx?/,
+				loaders: ["babel"],
+				include: path.resolve(ROOT_PATH, "app")
+			}
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin()
+		]
+	});
+}
