1457f6c006fa3845d8515b2da4eef29412ceded0
jorda0mega <jorda0mega@gmail.com>
2015-09-01 19:28:46 -0400

    chapter 5

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,32 @@
+import AltContainer from 'alt/AltContainer';
 import React from "react";
-import Note from "./Note.jsx";
+import Notes from "./Notes.jsx";
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
 	render() {
-		return <Note />;
+		return (
+			<div>
+				<button className='add-note' onClick={this.addNote}>+</button>
+				<AltContainer
+					stores={[NoteStore]}
+					inject={ {
+            items: () => NoteStore.getState().notes
+          } }
+					>
+					<Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+				</AltContainer>
+			</div>
+		);
+	}
+	addNote() {
+		NoteActions.create({task: 'New task'});
+	}
+	editNote(id, task){
+		NoteActions.update({id, task});
+	}
+	deleteNote(id){
+		NoteActions.delete(id);
 	}
 }
